61e7e6bae5718e8e4bcda56417ddf9a098e2098d
tgummerer <t.gummerer@gmail.com>
2012-04-28 23:54:31 +0200

    git-convert-index keeps offsets to directory entries, which can later be used to write in the offsets to the files

--- git-convert-index.py
+++ git-convert-index.py
@@ -2,7 +2,9 @@ import socket
 import hashlib
 import binascii
 import struct
+import os.path
 
+# fread {{{
 f = open(".git/index", "rb")
 filedata = ""
 def fread(n):
@@ -13,18 +15,35 @@ def fread(n):
     else:
         filedata += data
     return data
+# }}}
 
+# fwrite {{{
+fw = open(".git/index-v4", "wb")
+writtenbytes = 0
+def fwrite(data):
+    global writtenbytes
+    writtenbytes += len(data)
+    fw.write(data)
+# }}}
+
+# convert {{{
 def convert(n):
     return str(struct.unpack('!I', n)[0])
+# }}}
 
+# readheader {{{
 def readheader(f):
     # Signature
     signature = fread(4)
     header = struct.unpack('!II', fread(8))
     return dict({"signature": signature, "version": header[0], "nrofentries": header[1]})
+# }}}
 
+# readindexentries {{{
 def readindexentries(f):
     indexentries = []
+    paths = set()
+    files = set()
     byte = fread(1)
     i = 0
     # Read index entries
@@ -42,6 +61,10 @@ def readindexentries(f):
         while byte != '\0':
             string = string + byte
             byte = fread(1)
+
+
+        paths.add(os.path.dirname(string))
+        files.add(os.path.basename(string))
 
         entry = entry + (string, )                              # Filename
 
@@ -61,8 +84,10 @@ def readindexentries(f):
 
         i = i + 1
 
-    return indexentries, byte
+    return indexentries, byte, paths, files
+# }}}
 
+# readextensiondata {{{
 def readextensiondata(f):
     extensionsize = fread(4)
 
@@ -70,6 +95,7 @@ def readextensiondata(f):
     subtreenr = [0]
     subtree = [""]
     listsize = 0
+    extensiondata = dict()
     while read < int(convert(extensionsize)):
         path = ""
         byte = fread(1)
@@ -118,16 +144,20 @@ def readextensiondata(f):
         else:
             sha1 = "invalid"
 
-        extensiondata.append(dict({"path": fpath, "entry_count": entry_count,
-            "subtrees": subtrees, "sha1": sha1}))
+        extensiondata[fpath] = dict({"path": fpath, "entry_count": entry_count,
+            "subtrees": subtrees, "sha1": sha1})
 
     return extensiondata
+# }}}
 
+# printheader {{{
 def printheader(header):
     print "Signature: " + header["signature"]
     print "Version: " + str(header["version"])
     print "Number of entries: " + str(header["nrofentries"])
+# }}}
 
+# printindexentries {{{
 def printindexentries(indexentries):
     for entry in indexentries:
         print entry["filename"]
@@ -136,32 +166,73 @@ def printindexentries(indexentries):
         print "  dev: " + str(entry["dev"]) + "\tino: " + str(entry["ino"])
         print "  uid: " + str(entry["uid"]) + "\tgid: " + str(entry["gid"])
         print "  size: " + str(entry["filesize"]) + "\tflags: " + "%x" % entry["flags"]
+# }}}
 
+# {{{ printextensiondata
 def printextensiondata(extensiondata):
-    for entry in extensiondata:
+    for entry in extensiondata.viewvalues():
         print entry["sha1"] + " " + entry["path"] + " (" + entry["entry_count"] + " entries, " + entry["subtrees"] + " subtrees)"
+# }}}
 
-fw = open(".git/index-v4", "wb")
+# writeheader {{{
+def writeheader(f, header, paths, files):
+    fwrite(header["signature"])
+    fwrite(struct.pack("!IIII", header["version"], len(paths), len(files), header["nrofentries"]))
+# }}}
+
+# writedirectories {{{
+def writedirectories(f, paths, treeextensiondata):
+    offsets = dict()
+    for p in paths:
+        offsets[p] = writtenbytes
+        fwrite(struct.pack("!Q", 0))
+        fwrite(p + "\0")
+        p += "/"
+        if p in treeextensiondata:
+            fwrite(struct.pack("!ll", int(treeextensiondata[p]["entry_count"]), int(treeextensiondata[p]["subtrees"])))
+            if (treeextensiondata[p]["entry_count"] != "-1"):
+                fwrite(binascii.unhexlify(treeextensiondata[p]["sha1"]))
+
+        else: # If there is no cache-tree data we assume the entry is invalid
+            # TODO: Subtree stuff
+            fwrite(struct.pack("!II", -1, 0))
+    return offsets
+# }}}
 
 header = readheader(f)
 
-indexentries, byte = readindexentries(f)
+indexentries, byte, paths, files = readindexentries(f)
 
 sup = fread(3)
 byte = byte + sup
 extensiondata = []
 
 if byte == "TREE":
-    extensiondata = readextensiondata(f)
+    treeextensiondata = readextensiondata(f)
 
-printheader(header)
-printindexentries(indexentries)
-printextensiondata(extensiondata)
+# printheader(header)
+# printindexentries(indexentries)
+# printextensiondata(extensiondata)
 
 sha1 = hashlib.sha1()
 sha1.update(filedata)
 print "SHA1 over filedata: " + str(sha1.hexdigest())
 
-sha1 = f.read(20)
-print "SHA1 over the whole file: " + str(binascii.hexlify(sha1))
+sha1read = f.read(20)
+print "SHA1 over the whole file: " + str(binascii.hexlify(sha1read))
+
+if sha1.hexdigest() == binascii.hexlify(sha1read):
+    writeheader(fw, header, paths, files)
+    offsets = writedirectories(fw, sorted(paths), treeextensiondata)
+
+    # Test for the offsets
+    i = 1
+    for p in paths:
+        print offsets[p]
+        fw.seek(offsets[p])
+        fw.write(struct.pack("!Q", i))
+        i += 1
+
+    print "Write index"
+    # Write new index
 
