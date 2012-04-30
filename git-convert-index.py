56d31ee2b783f33fdd60facdfb3dcfd6df5c36c3
tgummerer <t.gummerer@gmail.com>
2012-04-30 22:09:24 +0200

    Reading REUC extension

--- git-convert-index.py
+++ git-convert-index.py
@@ -156,6 +156,51 @@ def readextensiondata(f):
     return extensiondata
 # }}}
 
+# readreucextensiondata {{{
+def readreucextensiondata(f):
+    extensionsize = fread(4)
+
+    read = 0
+    extensiondata = list()
+    while read < int(convert(extensionsize)):
+        path = ""
+        byte = fread(1)
+        read += 1
+        while byte != '\0':
+            path += byte
+            byte = fread(1)
+            read += 1
+
+        entry_mode = list()
+        i = 0
+        while i < 3:
+            byte = fread(1)
+            read += 1
+            nr = ""
+            while byte != '\0':
+                nr += byte
+                byte = fread(1)
+                read += 1
+            i += 1
+
+            entry_mode.append(int(nr))
+
+        i = 0
+        obj_names = list()
+        while i < 3:
+            if entry_mode[i] != 0:
+                obj_names.append(fread(20))
+                read += 20
+            else:
+                obj_names[i] = ""
+            i += 1
+
+        extensiondata.append(dict({"path": path, "entry_mode0": entry_mode[0], "entry_mode1": entry_mode[1], "entry_mode2": entry_mode[2], "obj_names0": obj_names[0], "obj_names1": obj_names[1], "obj_names2": obj_names[2]}))
+
+    return extensiondata
+
+# }}}
+
 # printheader {{{
 def printheader(header):
     print "Signature: " + header["signature"]
@@ -183,14 +228,23 @@ def printextensiondata(extensiondata):
         print entry["sha1"] + " " + entry["path"] + " (" + entry["entry_count"] + " entries, " + entry["subtrees"] + " subtrees)"
 # }}}
 
-# writeheader {{{
-def writeheader(header, paths, files):
-    fwrite(header["signature"])
-    fwrite(struct.pack("!IIII", header["version"], len(paths), len(files), header["nrofentries"]))
+# printreucextensiondata {{{
+def printreucextensiondata(extensiondata):
+    for e in extensiondata:
+        print "Path: " + e["path"]
+        print "Entrymode 1: " + str(e["entry_mode0"]) + " Entrymode 2: " + str(e["entry_mode1"]) + " Entrymode 3: " + str(e["entry_mode2"])
+        print "Objectnames 1: " + binascii.hexlify(e["obj_names0"]) + " Objectnames 2: " + binascii.hexlify(e["obj_names1"]) + " Objectnames 3: " + binascii.hexlify(e["obj_names2"])
 # }}}
 
-# writedirectories {{{
-def writedirectories(paths, treeextensiondata):
+# Write stuff for index-v4 draft0 {{{
+# writev4_0header {{{
+def writev4_0header(header, paths, files):
+    fwrite(header["signature"])
+    fwrite(struct.pack("!IIIIQ", header["version"], len(paths), len(files), header["nrofentries"], 0))
+# }}}
+
+# writev4_0directories {{{
+def writev4_0directories(paths, treeextensiondata):
     offsets = dict()
     subtreenr = dict()
     # Calculate subtree numbers
@@ -220,12 +274,15 @@ def writedirectories(paths, treeextensiondata):
                 fwrite(binascii.unhexlify(treeextensiondata[p]["sha1"]))
 
         else: # If there is no cache-tree data we assume the entry is invalid
-            fwrite(struct.pack("!II", -1, subtreenr[p]))
+            if p != "/":
+                fwrite(struct.pack("!ii", -1, subtreenr[p]))
+            else:
+                fwrite(struct.pack("!ii", -1, subtreenr[""]))
     return offsets
 # }}}
 
-# writefiles {{{
-def writefiles(files):
+# writev4_0files {{{
+def writev4_0files(files):
     offsets = dict()
     for f in files:
         offsets[f] = writtenbytes
@@ -234,8 +291,8 @@ def writefiles(files):
     return offsets
 # }}}
 
-# writefileentries {{{
-def writefileentries(entries, fileoffsets):
+# writev4_0fileentries {{{
+def writev4_0fileentries(entries, fileoffsets):
     offsets = dict()
     for e in sorted(entries, key=lambda k: k['pathname']):
         if e["pathname"] not in offsets:
@@ -256,8 +313,8 @@ def writefileentries(entries, fileoffsets):
     return offsets
 # }}}
 
-# writefileoffsets {{{
-def writefileoffsets(diroffsets, fileoffsets, dircrcoffset):
+# writev4_0fileoffsets {{{
+def writev4_0fileoffsets(diroffsets, fileoffsets, dircrcoffset):
     for d in sorted(diroffsets):
         fw.seek(diroffsets[d])
         fw.write(struct.pack("!Q", fileoffsets[d]))
@@ -267,6 +324,14 @@ def writefileoffsets(diroffsets, fileoffsets, dircrcoffset):
     #data = f.read(dircrcoffset)
     fw.seek(dircrcoffset)
     fw.write(struct.pack("!I", binascii.crc32(writtendata) & 0xffffffff))
+
+# }}}
+
+# writev4_0reucextensiondata {{{
+def writev4_0reucextensiondata(data):
+    pass
+
+# }}}
 
 # }}}
 
@@ -282,33 +347,56 @@ header = readheader(f)
 
 indexentries, byte, paths, files = readindexentries(f)
 
-ext = fread(3)
-ext = byte + ext
+filedata = filedata[:-1]
+ext = byte + f.read(3)
 extensiondata = []
 
+ext2 = ""
 if ext == "TREE":
+    filedata += ext
     treeextensiondata = readextensiondata(f)
+    ext2 = f.read(4)
+else:
+    treeextensiondata = dict()
+
+if ext == "REUC" or ext2 == "REUC":
+    if ext == "REUC":
+        filedata += ext
+    else:
+        filedata += ext2
+    reucextensiondata = readreucextensiondata(f)
+else:
+    reucextensiondata = list()
 
 # printheader(header)
 # printindexentries(indexentries)
 # printextensiondata(extensiondata)
+# printreucextensiondata(reucextensiondata)
+
+
+if ext != "TREE" and ext != "REUC" and ext2 != "REUC":
+    sha1read = ext + f.read(16)
+elif ext2 != "REUC" and ext == "TREE":
+    sha1read = ext2 + f.read(16)
+else:
+    sha1read = f.read(20)
+
+print "SHA1 over the whole file: " + str(binascii.hexlify(sha1read))
 
 sha1 = hashlib.sha1()
 sha1.update(filedata)
 print "SHA1 over filedata: " + str(sha1.hexdigest())
 
-sha1read = f.read(20)
-print "SHA1 over the whole file: " + str(binascii.hexlify(sha1read))
-
 if sha1.hexdigest() == binascii.hexlify(sha1read):
-    writeheader(header, paths, files)
-    diroffsets = writedirectories(sorted(paths), treeextensiondata)
-    fileoffsets = writefiles(files)
+    writev4_0header(header, paths, files)
+    diroffsets = writev4_0directories(sorted(paths), treeextensiondata)
+    fileoffsets = writev4_0files(files)
     dircrcoffset = writtenbytes
 
     # TODO: Replace with something faster. Doesn't make sense to calculate crc32 here
     writecrc32()
-    fileoffsets = writefileentries(indexentries, fileoffsets)
-    writefileoffsets(diroffsets, fileoffsets, dircrcoffset)
+    fileoffsets = writev4_0fileentries(indexentries, fileoffsets)
+    writev4_0fileoffsets(diroffsets, fileoffsets, dircrcoffset)
+    writev4_0reucextensiondata(reucextensiondata)
 else:
     print "File is corrupted"
