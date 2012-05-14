785246609e7cdc5ab1c4e376d75c7cfef18a1487
tgummerer <t.gummerer@gmail.com>
2012-05-14 11:17:06 +0200

    Code refactoring

--- git-read-index-v5.py
+++ git-read-index-v5.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-# Outputs: All filenames (including path), sorted lexically. Output format is the same as git ls-files
+# Outputs: All filenames (including path), sorted lexically. Output format is
+# the same as git ls-files
+#
 # Usage: python git-read-index-v5.py
 # Convert index v2/v3 with git-convert-index.py
 
@@ -8,117 +10,144 @@ import struct
 import binascii
 
 f = open(".git/index-v5", "rb")
-filedata = list()
 
 
-def fread(n):
-    global filedata
+def read_calc_crc(n, partialcrc=0):
     data = f.read(n)
-    filedata.append(data)
-    return data
+    crc = binascii.crc32(data, partialcrc)
+    return data, crc
 
 
-def readheader(f):
+def read_header(f):
     global filedata
-    signature = fread(4)
-    header = struct.unpack('!IIII', fread(16))
+    signature, partialcrc = read_calc_crc(4)
+    readheader, partialcrc = read_calc_crc(16, partialcrc)
+    vnr, ndir, nfile, nextensions = struct.unpack('!IIII', readheader)
+
+    extoffsets = list()
+    for i in xrange(0, nextensions):
+        readoffset, partialcrc = read_calc_crc(4, partialcrc)
+        extoffsets.append(readoffset)
+
     crc = f.read(4)
-    readcrc = struct.pack("!i", binascii.crc32("".join(filedata)))
-    filedata = list()
-    if crc == readcrc:
-        return dict({"signature": signature, "vnr": header[0], "ndir": header[1], "nfile": header[2], "next": header[3]})
+    datacrc = struct.pack("!i", partialcrc)
+
+    if crc == datacrc:
+        return dict(signature=signature, vnr=vnr, ndir=ndir, nfile=nfile,
+                nextensions=nextensions, extoffsets=extoffsets)
     else:
         raise Exception("Wrong crc")
 
 
 def readindexentries(header):
-    f.seek(24 + header["ndir"] * 4)
-    directories = readdirs(header["ndir"])
+    # Skip header and directory offsets
+    f.seek(24 + header["nextensions"] * 4 + header["ndir"] * 4)
+
+    directories = read_dirs(header["ndir"])
     files, dirnr = readfiles(directories, 0, [])
     for fi in files:
         print fi["name"]
-    # printdirectories(directories)
 
 
 def readfiles(directories, dirnr, entries):
-    global filedata
-    f.seek(directories[dirnr]["foffset"])
-    offset = struct.unpack("!I", fread(4))[0]
-    f.seek(offset)
-    filedata = list()
+    if dirnr == 0:
+        f.seek(directories[dirnr]["foffset"])
+        (readoffset, partialcrc) = read_calc_crc(4)
+        (offset, ) = struct.unpack("!I", readoffset)
+        f.seek(offset)
+
     queue = list()
-    i = 0
-    while i < directories[dirnr]["nfiles"]:
-        filedata.append(struct.pack("!I", f.tell()))
+    for i in xrange(0, directories[dirnr]["nfiles"]):
+        # A little cheating here in favor of simplicity and execution speed.
+        # The fileoffset is only read when really needed, in the other cases
+        # it's just calculated from the file position, to save on reads and
+        # simplify the code.
+
+        if partialcrc != 0:
+            partialcrc = binascii.crc(struct.pack("!I", f.tell()))
+
         filename = ""
-        byte = fread(1)
+        (byte, partialcrc) = read_calc_crc(1, partialcrc)
         while byte != '\0':
             filename += byte
-            byte = fread(1)
+            (byte, partialcrc) = read_calc_crc(1, partialcrc)
 
-        data = struct.unpack("!HHIII", fread(16))
-        objhash = fread(20)
-        readcrc = struct.pack("!i", binascii.crc32("".join(filedata)))
+        (statdata, partialcrc) = read_calc_crc(16, partialcrc)
+        (flags, mode, mtimes, mtimens,
+                statcrc) = struct.unpack("!HHIII", statdata)
+
+        (objhash, partialcrc) = read_calc_crc(20, partialcrc)
+
+        datacrc = struct.pack("!i", partialcrc)
         crc = f.read(4)
-        if readcrc != crc:
-            print "Wrong CRC: " + filename
-        filedata = list()
+        if datacrc != crc:
+            raise Exception("Wrong CRC: " + filename)
 
-        i += 1
+        queue.append(dict(name=directories[dirnr]["pathname"] + filename,
+            flags=flags, mode=mode, mtimes=mtimes, mtimens=mtimens,
+            statcrc=statcrc, objhash=binascii.hexlify(objhash)))
 
-        queue.append(dict({"name": directories[dirnr]["pathname"] + filename, "flags": data[0], "mode": data[1], "mtimes": data[2], "mtimens": data[3], "statcrc": data[4], "objhash": binascii.hexlify(objhash)}))
+        partialcrc = 0
 
     if len(directories) > dirnr:
         i = 0
         while i < len(queue):
-            if len(directories) - 1 > dirnr and queue[i]["name"] > directories[dirnr + 1]["pathname"]:
+            if (len(directories) - 1 > dirnr and
+                    queue[i]["name"] > directories[dirnr + 1]["pathname"]):
                 entries, dirnr = readfiles(directories, dirnr + 1, entries)
             else:
                 entries.append(queue[i])
                 i += 1
+
         return entries, dirnr
 
 
-def readdirs(ndir):
-    global filedata
-    i = 0
+def read_dirs(ndir):
     dirs = list()
-    while i < ndir:
+    for i in xrange(0, ndir):
         pathname = ""
-        byte = fread(1)
+        (byte, partialcrc) = read_calc_crc(1)
         while byte != '\0':
             pathname += byte
-            byte = fread(1)
+            (byte, partialcrc) = read_calc_crc(1, partialcrc)
 
-        data = struct.unpack("!HIIIIII", fread(26))
-        objname = fread(20)
+        (readstatdata, partialcrc) = read_calc_crc(26, partialcrc)
+        (flags, foffset, cr, ncr, nsubtrees, nfiles,
+                nentries) = struct.unpack("!HIIIIII", readstatdata)
 
-        readcrc = struct.pack("!i", binascii.crc32("".join(filedata)))
+        (objname, partialcrc) = read_calc_crc(20, partialcrc)
+
+        datacrc = struct.pack("!i", partialcrc)
         crc = f.read(4)
-        filedata = list()
-        if crc != readcrc:
+        if crc != datacrc:
             raise Exception("Wrong crc for " + pathname)
 
-        dirs.append(dict({"pathname": pathname, "flags": data[0], "foffset": data[1], "cr": data[2], "ncr": data[3], "nsubtrees": data[4], "nfiles": data[5], "nentries": data[6], "objname": objname}))
-        i += 1
+        dirs.append(dict(pathname=pathname, flags=flags, foffset=foffset,
+            cr=cr, ncr=ncr, nsubtrees=nsubtrees, nfiles=nfiles,
+            nentries=nentries, objname=objname))
 
     return dirs
 
 
 def printheader(header):
-    print "Signature: " + header["signature"]
-    print "Version: " + str(header["vnr"])
-    print "Number of directories: " + str(header["ndir"])
-    print "Number of files: " + str(header["nfile"])
-    print "Number of extension: " + str(header["next"])
+    print("Signature: %(signature)s\t\t\tVersion: %(vnr)s\n"
+            "Number of directories: %(ndir)s\tNumber of files: %(nfile)s\n"
+            "Number of extensions: %(nextensions)s" % header)
 
 
 def printdirectories(directories):
     for d in directories:
-        print d["pathname"] + " " + str(d["flags"]) + " " + str(d["foffset"]) + " " + str(d["cr"]) + " " + str(d["ncr"]) + " " + str(d["nsubtrees"]) + " " + str(d["nfiles"]) + " " + str(d["nentries"]) + " " + str(binascii.hexlify(d["objname"]))
+        print ("path: %(pathname)s flags: %(flags)s foffset: %(foffset)s "
+                "ncr: %(ncr)s cr: %(cr)s nfiles: %(nfiles)s "
+                "nentries: %(nentries)s objname: " % d +
+                str(binascii.hexlify(d["objname"])))
 
 
-header = readheader(f)
-readindexentries(header)
-
+header = read_header(f)
 # printheader(header)
+
+if header["signature"] == "DIRC" and header["vnr"] == 5:
+    directories = readindexentries(header)
+else:
+    raise Exception("Signature or version of the index are wrong.\n"
+            "Header: %(signature)s\tVersion: %(vnr)s" % header)
