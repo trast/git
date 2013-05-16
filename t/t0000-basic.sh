a48122be29cc9088cc0aa388e9148bc62c1712dd
Thomas Rast <trast@inf.ethz.ch>
2013-05-16 22:50:14 +0200

    test-lib: verbose mode for only tests matching a pattern
    With the new --verbose-only=<pattern> option, one can enable --verbose
    at a per-test granularity.  The pattern is matched against the test
    number, e.g.
    
      ./t0000-basic.sh --verbose-only='2[0-2]'
    
    to see only the full output of test 20-22, while showing the rest in the
    one-liner format.
    
    As suggested by Jeff King, this takes care to wrap the entire
    test_expect_* block, but nothing else, in the verbose toggling.  We
    can use the test_start/end functions from the previous commit for the
    purpose.
    
    This is arguably not *too* useful on its own, but makes the next patch
    easier to follow.
    
    Helped-by: Jeff King <peff@peff.net>
    Signed-off-by: Thomas Rast <trast@inf.ethz.ch>

--- t/t0000-basic.sh
+++ t/t0000-basic.sh
@@ -225,15 +225,41 @@ test_expect_success 'test --verbose' '
 	test_expect_success "failing test" false
 	test_done
 	EOF
+	mv test-verbose/out test-verbose/out+
+	grep -v "^Initialized empty" test-verbose/out+ >test-verbose/out &&
 	check_sub_test_lib_test test-verbose <<-\EOF
 	> expecting success: true
 	> ok 1 - passing test
-	>
+	> Z
 	> expecting success: echo foo
 	> foo
-	>
 	> ok 2 - test with output
+	> Z
 	> expecting success: false
+	> not ok 3 - failing test
+	> #	false
+	> Z
+	> # failed 1 among 3 test(s)
+	> 1..3
+	EOF
+'
+
+test_expect_success 'test --verbose-only' '
+	test_must_fail run_sub_test_lib_test \
+		test-verbose-only-2 "test verbose-only=2" \
+		--verbose-only=2 <<-\EOF &&
+	test_expect_success "passing test" true
+	test_expect_success "test with output" "echo foo"
+	test_expect_success "failing test" false
+	test_done
+	EOF
+	check_sub_test_lib_test test-verbose-only-2 <<-\EOF
+	> ok 1 - passing test
+	> Z
+	> expecting success: echo foo
+	> foo
+	> ok 2 - test with output
+	> Z
 	> not ok 3 - failing test
 	> #	false
 	> # failed 1 among 3 test(s)
