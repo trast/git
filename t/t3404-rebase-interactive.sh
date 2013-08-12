4ababc19e3b68d9570467c70c6a8b5f05c07650e
Eric Sunshine <sunshine@sunshineco.com>
2013-08-12 00:07:37 -0400

    t3404: restore specialized rebase-editor following commentchar test
    At start of script, t3404 installs a specialized test-editor ($EDITOR)
    upon which many of the interactive rebase tests depend.  Late in t3404,
    test "rebase -i respects core.commentchar" installs its own custom
    editor but neglects to restore the specialized editor when finished.
    This oversight will cause later tests, which require the specialized
    editor, to fail.  (There are no such tests presently, but a subsequent
    patch will introduce one.)  Fix this problem.
    
    Signed-off-by: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3404-rebase-interactive.sh
+++ t/t3404-rebase-interactive.sh
@@ -949,6 +949,7 @@ test_expect_success 'rebase -i respects core.commentchar' '
 	sed -e "2,\$s/^/\\\\/" "$1" >"$1.tmp" &&
 	mv "$1.tmp" "$1"
 	EOF
+	test_when_finished "set_fake_editor" &&
 	test_set_editor "$(pwd)/remove-all-but-first.sh" &&
 	git rebase -i B &&
 	test B = $(git cat-file commit HEAD^ | sed -ne \$p)
